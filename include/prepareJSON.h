54b30bc973b321a1e99c42a23ef12e46af7447c1
Frostzeichen <cruztjomar@gmail.com>
2023-11-30 04:09:00 +0800

    Feature: fit output to schema

--- include/prepareJSON.h
+++ include/prepareJSON.h
@@ -5,15 +5,19 @@
 String prepareJSON() {
   String jsonResponse;
   jsonResponse.reserve(1024);
-  jsonResponse = F("HTTP/1.1 200 OK\r\n"
+  jsonResponse = F("HTTP/1.1 200 OK\r\n"  // This code is designed for 1:1 sensor:controller only. TODO: add support for multiple sensors.
                "Content-Type: application/json\r\n"
                "Connection: close\r\n"
                "Refresh: 5\r\n"
                "\r\n"
-               "{\"photoresistor\":");
+               "{"
+               "\"sensorModel\": \"photoresistor\", \"reading\": "); // sensor data
   jsonResponse += analogRead(A0);
-  jsonResponse += F("}"
-                "\r\n");
+  jsonResponse += F(","
+               "\"type\": \"light\", \"format\": \"analog\", " // sensor data
+               "\"controllerModel\": \"NodeMCU v0.9\"" // controller data
+               "}"
+               "\r\n");
   return jsonResponse;
 }
 
